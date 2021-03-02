ada5a40b67bc560f3691c94f20eddffb23ba2a00
Eadmond <eadmond@hotmail.com>
2021-03-02 15:47:54 -0800

    Added TextMeshPro
    And improved the tile a little

---
+++ Assets/Scripts/CoordinateLabel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteAlways]
+public class CoordinateLabel : MonoBehaviour
+{
+    TextMeshPro tmpObj;
+    Vector2Int position = new Vector2Int();
+
+    void Awake()
+    {
+        tmpObj = GetComponent<TextMeshPro>();
+        DisplayCurrentCoordinates();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            DisplayCurrentCoordinates();
+        }   
+    }
+
+    void DisplayCurrentCoordinates()
+    {
+        position.x = Mathf.RoundToInt(transform.parent.position.x);
+        position.y = Mathf.RoundToInt(transform.parent.position.y);
+        tmpObj.text = position.x / 10 + "," + position.y/10;
+    }
+}
