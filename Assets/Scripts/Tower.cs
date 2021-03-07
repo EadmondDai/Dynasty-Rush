16f1761baaffb1da4b4a2556e55f2c8d68de0c41
Eadmond <eadmond@hotmail.com>
2021-03-06 16:57:36 -0800

    Finishing a little

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -8,9 +8,38 @@ public class Tower : MonoBehaviour
     [SerializeField] int goldNeed;
     public int GoldNeed {get { return goldNeed; } }
 
+    [SerializeField] float buildDelay = 2.0f;
+
+    void Start()
+    {
+        StartCoroutine(Build());   
+    }
+
     public Tower CreateTower(Tower prefab, Vector3 position)
     {
         var towObj = Instantiate(prefab, position, Quaternion.identity);
         return towObj;
+    }
+
+    IEnumerator Build()
+    {
+        foreach(Transform child in transform)
+        {
+            transform.gameObject.SetActive(false);
+            foreach(Transform grandChild in child)
+            {
+                transform.gameObject.SetActive(false);
+            }
+        }
+
+        foreach (Transform child in transform)
+        {
+            transform.gameObject.SetActive(true);
+            yield return new WaitForSeconds(buildDelay);
+            foreach (Transform grandChild in child)
+            {
+                transform.gameObject.SetActive(true);
+            }
+        }
     }
 }
